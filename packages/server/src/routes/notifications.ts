80e8ee5f5eb66dff3fc79daebb23cc579d90692c
Francois Best <github@francoisbest.com>
2022-12-07 10:53:30 +0100

    fix: Use a single DB listening connection

--- packages/server/src/routes/notifications.ts
+++ packages/server/src/routes/notifications.ts
@@ -3,11 +3,19 @@ import {
   PublicKeyAuthHeaders,
   WebsocketNotificationTypes,
 } from '@e2esdk/api'
+import mitt from 'mitt'
 import { zodToJsonSchema } from 'zod-to-json-schema'
-import { notifyOfInsertsFor } from '../database/models/sharedKey.js'
+import { sharedKeyInsertsNotificationChannel } from '../database/models/sharedKey.js'
 import type { App } from '../types'
 
-export default async function infoRoutes(app: App) {
+export default async function notificationsRoutes(app: App) {
+  // We use a central EventEmitter pattern to have only a single
+  // listening connection to the database, then fan out here
+  // to forward to the correct websocket connection.
+  const emitter = mitt()
+  app.db.listen(sharedKeyInsertsNotificationChannel, toUserId =>
+    emitter.emit(`sharedKeyInserted:${toUserId}`, null)
+  )
   app.get<{
     Querystring: PublicKeyAuthHeaders
   }>(
@@ -22,15 +30,16 @@ export default async function infoRoutes(app: App) {
       },
     },
     async function websocketNotifications(connection, req) {
-      const channel = notifyOfInsertsFor(req.identity.userId)
-      const listener = await app.db.listen(channel, () => {
+      const sharedKeyForMe = `sharedKeyInserted:${req.identity.userId}}`
+      function onSharedKeyInsertedForMe() {
         connection.socket.send(WebsocketNotificationTypes.sharedKeyAdded)
-      })
+      }
+      emitter.on(sharedKeyForMe, onSharedKeyInsertedForMe)
       connection.on('close', () => {
         req.log.info(
           'Websocket closed by client, unregistering database listener'
         )
-        listener.unlisten()
+        emitter.off(sharedKeyForMe, onSharedKeyInsertedForMe)
       })
     }
   )
