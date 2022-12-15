ff5b2bc070cca22d624bae0ed40b5fa1423790a0
Francois Best <github@francoisbest.com>
2022-12-15 11:30:51 +0100

    chore: Don't publish server code to NPM
    Instead, run semantic-release on private packages,
    which will not publish them on NPM, but allow
    using the `publish` lifecycle hook on our custom plugin.
    
    There, we can detect an intent to release the server,
    which we store into the `server-needs-publishing` file
    which Docker can pick up and decide whether to proceed.

--- config/semantic-release/src/index.ts
+++ config/semantic-release/src/index.ts
@@ -9,14 +9,14 @@ const serverContextSchema = z.object({
   }),
 })
 
-export async function success(_pluginConfig: any, context: any) {
+export async function publish(_pluginConfig: any, context: any) {
   const ctx = serverContextSchema.safeParse(context)
   if (!ctx.success) {
     return
   }
   const serverVersion = ctx.data.nextRelease.version
   const repoRoot = path.resolve(ctx.data.cwd, '../..')
-  const serverInfoFile = path.resolve(repoRoot, 'server-was-published')
+  const serverInfoFile = path.resolve(repoRoot, 'server-needs-publishing')
   await fs.writeFile(serverInfoFile, serverVersion)
   context.logger.log(
     `Wrote server version ${serverVersion} to ${serverInfoFile} for Docker to pick up and push image`
