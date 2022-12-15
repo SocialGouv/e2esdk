c67b8c93273140a5f497e88c17207b11c7f673a9
Francois Best <github@francoisbest.com>
2022-12-15 01:25:12 +0100

    chore: Add custom SemRel plugin for NPM -> Docker
    It detects when the server package is published on NPM
    and writes the published version to a file, that the
    deployment workflow can detect to know whether
    to build and push a Docker image.

---
+++ config/semantic-release/src/index.ts
@@ -0,0 +1,24 @@
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import { z } from 'zod'
+
+const serverContextSchema = z.object({
+  cwd: z.string().endsWith('/packages/server'),
+  nextRelease: z.object({
+    version: z.string(),
+  }),
+})
+
+export async function success(_pluginConfig: any, context: any) {
+  const ctx = serverContextSchema.safeParse(context)
+  if (!ctx.success) {
+    return
+  }
+  const serverVersion = ctx.data.nextRelease.version
+  const repoRoot = path.resolve(ctx.data.cwd, '../..')
+  const serverInfoFile = path.resolve(repoRoot, 'server-was-published')
+  await fs.writeFile(serverInfoFile, serverVersion)
+  context.logger.log(
+    `Wrote server version ${serverVersion} to ${serverInfoFile} for Docker to pick up and push image`
+  )
+}
