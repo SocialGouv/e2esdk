b68bd3726ce29a54346b131b62136a587576afaa
Francois Best <github@francoisbest.com>
2022-12-15 17:40:12 +0100

    fix: Keep caret ranges when resolving versions

--- config/semantic-release/src/index.ts
+++ config/semantic-release/src/index.ts
@@ -9,28 +9,6 @@ const serverContextSchema = z.object({
   }),
 })
 
-export async function prepare(_: any, context: any) {
-  const ctx = serverContextSchema.safeParse(context)
-  if (!ctx.success) {
-    return
-  }
-  // Inject the version number into package.json
-  // This would otherwise be done by the @semantic-release/npm plugin,
-  // but the server is a private package so it won't be touched.
-  const serverPackageJsonFile = path.resolve(ctx.data.cwd, 'package.json')
-  const serverPackageJson = JSON.parse(
-    await fs.readFile(serverPackageJsonFile, { encoding: 'utf8' })
-  )
-  context.logger.log(
-    `Server package.json version: ${serverPackageJson.version} -> ${ctx.data.nextRelease.version}`
-  )
-  serverPackageJson.version = ctx.data.nextRelease.version
-  await fs.writeFile(
-    serverPackageJsonFile,
-    JSON.stringify(serverPackageJson, null, 2)
-  )
-}
-
 export async function publish(_: any, context: any) {
   const ctx = serverContextSchema.safeParse(context)
   if (!ctx.success) {
@@ -40,7 +18,7 @@ export async function publish(_: any, context: any) {
   const repoRoot = path.resolve(ctx.data.cwd, '../..')
   const serverInfoFile = path.resolve(repoRoot, 'server-needs-publishing')
   await fs.writeFile(serverInfoFile, serverVersion)
-  context.logger.log(
+  console.log(
     `Wrote server version ${serverVersion} to ${serverInfoFile} for Docker to pick up and push image`
   )
 }
