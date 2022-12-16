4902998f32347d18f2d7a8bb49fc572a1c1e4c5e
Francois Best <github@francoisbest.com>
2022-12-16 09:27:10 +0100

    chore: Better NPM summary

--- config/semantic-release/src/index.ts
+++ config/semantic-release/src/index.ts
@@ -2,8 +2,8 @@ import fs from 'node:fs/promises'
 import path from 'node:path'
 import { z } from 'zod'
 
-const serverContextSchema = z.object({
-  cwd: z.string().endsWith('/packages/server'),
+const contextSchema = z.object({
+  cwd: z.string(),
   nextRelease: z.object({
     name: z.string(),
     version: z.string(),
@@ -16,34 +16,32 @@ const serverContextSchema = z.object({
 })
 
 export async function publish(_: any, context: any) {
-  const ctx = serverContextSchema.safeParse(context)
-  if (!ctx.success) {
+  const ctx = contextSchema.safeParse(context)
+  if (!ctx.success || !ctx.data.cwd.endsWith('packages/server')) {
     return
   }
   const serverVersion = ctx.data.nextRelease.version
   const repoRoot = path.resolve(ctx.data.cwd, '../..')
   const serverInfoFile = path.resolve(repoRoot, 'server-needs-publishing')
   await fs.writeFile(serverInfoFile, serverVersion)
-  console.log(
-    `Wrote server version ${serverVersion} to ${serverInfoFile} for Docker to pick up and push image`
+  const summaryLine = `Bumped server version to \`${serverVersion}\`  \n`
+  await fs.appendFile(
+    path.resolve(repoRoot, 'GITHUB_STEP_SUMMARY_SERVER'),
+    summaryLine
   )
 }
 
 export async function success(_: any, context: any) {
-  const ctx = serverContextSchema.omit({ cwd: true }).safeParse(context)
-  if (!ctx.success) {
+  const ctx = contextSchema.safeParse(context)
+  if (!ctx.success || ctx.data.cwd.endsWith('packages/server')) {
     return
   }
+  const repoRoot = path.resolve(ctx.data.cwd, '../..')
   const { gitTag, name, version } = ctx.data.nextRelease
   const nameWithoutVersion = name.replace(`@${version}`, '')
-  const summaryLine =
-    nameWithoutVersion === '@socialgouv/e2esdk-server'
-      ? `✨ &nbsp;Bumped server version to \`${version}\`  \n`
-      : `📦 &nbsp;Published: [\`${gitTag}\`](https://www.npmjs.com/package/${nameWithoutVersion}/v/${version})  \n`
-
-  try {
-    await fs.appendFile(ctx.data.env.GITHUB_STEP_SUMMARY, summaryLine)
-  } catch (error) {
-    console.error(error)
-  }
+  const summaryLine = `- [\`${gitTag}\`](https://www.npmjs.com/package/${nameWithoutVersion}/v/${version})  \n`
+  await fs.appendFile(
+    path.resolve(repoRoot, 'GITHUB_STEP_SUMMARY_PACKAGES'),
+    summaryLine
+  )
 }
