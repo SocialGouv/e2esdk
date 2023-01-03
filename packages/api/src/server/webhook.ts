ec7abe3c05a8ccc860b7babcec353e7f9d00ce3a
Francois Best <github@francoisbest.com>
2023-01-03 15:06:59 +0100

    feat: Add webhook API definitions

---
+++ packages/api/src/server/webhook.ts
@@ -0,0 +1,43 @@
+import { z } from 'zod'
+import { publicKeyAuthHeaders } from '../headers'
+import { postKeychainItemRequestBody } from '../keychain'
+import { identitySchema } from '../schemas/identity'
+import { postSharedKeyBody } from '../sharedKey'
+
+export const webhookHeaders = publicKeyAuthHeaders.extend({
+  'x-e2esdk-server-pubkey': identitySchema.shape.signaturePublicKey,
+  'x-e2esdk-request-id': z.string(),
+  origin: z.string().url(),
+})
+
+export type WebhookHeaders = z.infer<typeof webhookHeaders>
+
+// --
+
+export const webhookRoutes = {
+  // Authorizations
+  '/authorize/signup': {
+    body: identitySchema.pick({ userId: true }),
+  },
+  '/authorize/key-share': {
+    body: postSharedKeyBody,
+  },
+  // Notifications
+  '/notify/signup': {
+    body: identitySchema,
+  },
+  '/notify/key-added': {
+    body: postKeychainItemRequestBody.omit({
+      name: true,
+      payload: true,
+      subkeyIndex: true,
+      signature: true,
+    }),
+  },
+} as const
+
+export type WebhookRoutes = {
+  [Path in keyof typeof webhookRoutes]: {
+    body: z.infer<typeof webhookRoutes[Path]['body']>
+  }
+}
