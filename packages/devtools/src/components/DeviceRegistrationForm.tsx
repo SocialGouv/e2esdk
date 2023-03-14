d08a4e5167b0abd9b2738b2ae2a516badfef9ad9
Francois Best <github@francoisbest.com>
2023-03-14 14:06:31 +0100

    fix: QR code URI manual entry in devtools

--- packages/devtools/src/components/DeviceRegistrationForm.tsx
+++ packages/devtools/src/components/DeviceRegistrationForm.tsx
@@ -2,12 +2,12 @@ import {
   Box,
   Button,
   FormControl,
+  FormHelperText,
   FormLabel,
   Input,
   Stack,
   StackProps,
 } from '@chakra-ui/react'
-import { base64Bytes, identitySchema } from '@socialgouv/e2esdk-api'
 import { useE2ESDKClient } from '@socialgouv/e2esdk-react'
 import React from 'react'
 import { useForm } from 'react-hook-form'
@@ -15,9 +15,7 @@ import { FiSave } from 'react-icons/fi'
 import { z } from 'zod'
 
 const formSchema = z.object({
-  userId: identitySchema.shape.userId,
-  deviceId: z.string().uuid(),
-  deviceSecret: base64Bytes(32),
+  qr: z.string(),
 })
 
 type FormValues = z.infer<typeof formSchema>
@@ -31,11 +29,7 @@ export const DeviceRegistrationForm: React.FC<
   const { register, handleSubmit, reset } = useForm<FormValues>()
   const onSubmit = React.useCallback(
     (values: FormValues) => {
-      client.registerEnrolledDevice(
-        values.userId,
-        values.deviceId,
-        values.deviceSecret
-      )
+      client.registerEnrolledDevice(values.qr)
       reset()
     },
     [client, reset]
@@ -44,16 +38,9 @@ export const DeviceRegistrationForm: React.FC<
     <Box as="form" onSubmit={handleSubmit(onSubmit)} px={5}>
       <Stack spacing={4} {...props}>
         <FormControl>
-          <FormLabel>User ID</FormLabel>
-          <Input fontFamily="mono" {...register('userId')} />
-        </FormControl>
-        <FormControl>
-          <FormLabel>Device ID</FormLabel>
-          <Input fontFamily="mono" {...register('deviceId')} />
-        </FormControl>
-        <FormControl>
-          <FormLabel>Device secret</FormLabel>
-          <Input fontFamily="mono" {...register('deviceSecret')} />
+          <FormLabel>Device registration URI</FormLabel>
+          <Input fontFamily="mono" {...register('qr')} />
+          <FormHelperText>// todo: Add QR code scanner</FormHelperText>
         </FormControl>
         <Button type="submit" leftIcon={<FiSave />}>
           Register device
