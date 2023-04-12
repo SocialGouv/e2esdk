3c2c9935d1a0841134a62dbb2d914d5193c8ea8e
Matéo Mévollon <m.mevollon@proton.me>
2023-04-12 12:03:53 +0200

    fix: release

--- packages/api/src/signup.ts
+++ packages/api/src/signup.ts
@@ -3,7 +3,6 @@ import { identitySchema } from './schemas/identity'
 
 // Request and response have the same shape:
 // the server echoes its input, allowing the
-// client to verify what was stored and authenticate
-// the server.
+// client to verify what was stored and authenticate the server.
 export const signupBody = identitySchema
 export type SignupBody = z.infer<typeof signupBody>
