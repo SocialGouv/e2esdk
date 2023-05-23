19733830921d92d153eaef17273fbd15ef41d95b
Francois Best <github@francoisbest.com>
2023-05-23 14:51:19 +0200

    docs: Fingerprint & OPAQUE API docs

--- packages/crypto/src/sodium/fingerprint.ts
+++ packages/crypto/src/sodium/fingerprint.ts
@@ -1,5 +1,16 @@
 import type { Sodium } from './sodium'
 
+/**
+ * Hash an input value into a constant-length output
+ *
+ * @param input Either a UTF8 encoded string or a buffer of bytes
+ * @returns a base64url encoded string of the 32 byte hash of the input.
+ *
+ * Algorithm: BLAKE2b
+ * Note: Because the underlying hash computation is very fast,
+ * this technique should not be used on low-entropy inputs,
+ * solely user-provided inputs, or inputs that can be enumerated somehow.
+ */
 export function fingerprint(sodium: Sodium, input: string | Uint8Array) {
   return sodium.crypto_generichash(32, input, null, 'base64')
 }
--- packages/crypto/src/sodium/opaque.ts
+++ packages/crypto/src/sodium/opaque.ts
@@ -1,6 +1,21 @@
 import { SecretBoxCipher } from './ciphers'
 import { Sodium } from './sodium'
 
+/**
+ * Derive the OPAQUE export key into a symmetric encryption cipher
+ *
+ * An OPAQUE key agreement is performed when signing up and logging into e2esdk,
+ * which generates an export key on the client only. This export key is used
+ * to wrap the mainKey associated with the user identity.
+ *
+ * Each device enrolled by the user brings its own export key, and therefore
+ * a copy of the wrapped main key exists per-device, allowing revoking devices
+ * easily, as well as rotating device credentials with minimal server updates.
+ *
+ * @param exportKey The OPAQUE client export key obtained after
+ *   completing a registration or login flow
+ * @returns a symmetric Cipher to encrypt/decrypt the mainKey for this device.
+ */
 export function getOpaqueExportCipher(
   sodium: Sodium,
   exportKey: Uint8Array
