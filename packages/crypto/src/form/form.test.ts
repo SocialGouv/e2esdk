2cf692dc7d5c3347146dd2e4aa95c9f406fec38b
Francois Best <github@francoisbest.com>
2023-07-11 10:34:43 +0200

    test: Add form encryption unit tests

---
+++ packages/crypto/src/form/form.test.ts
@@ -0,0 +1,127 @@
+import { base64UrlDecode, base64UrlEncode } from '../shared/codec'
+import { ready, sodium } from '../sodium/sodium'
+import {
+  decryptFormData,
+  decryptFormForEdition,
+  encryptFormData,
+} from './encryption'
+import {
+  clearEncryptedFormLocalState,
+  generateEncryptedFormLocalState,
+  initializeEncryptedFormLocalState,
+  isEncryptedFormLocalStatePersisted,
+  listPersistedEncryptedFormLocalStates,
+  persistEncryptedFormLocalState,
+} from './state'
+
+const localStorageMock = (function () {
+  let store: Record<string, string> = {}
+  return {
+    get length() {
+      return Object.keys(store).length
+    },
+    getItem(key: string) {
+      return store[key]
+    },
+    setItem(key: string, value: string) {
+      store[key] = value
+    },
+    clear() {
+      store = {}
+    },
+    removeItem(key: string) {
+      delete store[key]
+    },
+    key(index: number) {
+      return Object.keys(store)[index]
+    },
+    getAll() {
+      return store
+    },
+  }
+})()
+
+const TEST_KEY_PAIR = {
+  publicKey: base64UrlDecode('QV4W8xDJLzmdlnFFobppKWQb2WbOEAGR2lrPQogNXQA'),
+  privateKey: base64UrlDecode('bd0d1TLbthKjfc5ZiIdUu5cQ88LHVHcSq8-v9wIfMW0'),
+}
+
+const TEST_DATA = {
+  firstName: 'James',
+  lastName: 'Brown',
+  age: 73,
+  dateOfBirth: new Date('1933-05-03'),
+  isAlive: false,
+  occupations: ['singer', 'musician', 'record producer', 'band leader'],
+  children: {
+    min: 9,
+    max: 13,
+  },
+}
+
+beforeAll(async () => {
+  Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+  await ready
+  const publicKey = sodium.crypto_scalarmult_base(TEST_KEY_PAIR.privateKey)
+  expect(publicKey).toEqual(TEST_KEY_PAIR.publicKey)
+})
+
+describe('forms', () => {
+  test('state init', async () => {
+    const state = await generateEncryptedFormLocalState(TEST_KEY_PAIR.publicKey)
+    expect(state.formPublicKey).toEqual(TEST_KEY_PAIR.publicKey)
+    expect(state.keyDerivationContext).toEqual('QV4W8xDJ')
+    expect(state.sodium.ready).resolves.toBe(void 0)
+    // States are not persisted by default
+    expect(listPersistedEncryptedFormLocalStates()).toEqual([])
+  })
+
+  test('state retrieval', async () => {
+    const namespace = 'namespace'
+    const state1 = await generateEncryptedFormLocalState(
+      TEST_KEY_PAIR.publicKey
+    )
+    persistEncryptedFormLocalState(state1, namespace)
+    expect(
+      window.localStorage.getItem('e2esdk:forms:localState:namespace')
+    ).toEqual(base64UrlEncode(state1.mainSecret))
+    const state2 = await initializeEncryptedFormLocalState(
+      namespace,
+      TEST_KEY_PAIR.publicKey
+    )
+    expect(state2.mainSecret).toEqual(state1.mainSecret)
+    expect(state2.formPublicKey).toEqual(state1.formPublicKey)
+    expect(state2.identity.privateKey).toEqual(state1.identity.privateKey)
+    expect(state2.keyDerivationSecret).toEqual(state1.keyDerivationSecret)
+    expect(listPersistedEncryptedFormLocalStates()).toEqual([namespace])
+    expect(isEncryptedFormLocalStatePersisted(namespace)).toBe(true)
+  })
+
+  test('state cleanup', async () => {
+    const namespace = 'namespace'
+    const state = await generateEncryptedFormLocalState(TEST_KEY_PAIR.publicKey)
+    persistEncryptedFormLocalState(state, namespace)
+    expect(listPersistedEncryptedFormLocalStates()).toEqual([namespace])
+    expect(isEncryptedFormLocalStatePersisted(namespace)).toBe(true)
+    clearEncryptedFormLocalState(namespace)
+    expect(listPersistedEncryptedFormLocalStates()).toEqual([])
+    expect(isEncryptedFormLocalStatePersisted(namespace)).toBe(false)
+  })
+
+  test('encrypting data, decryption via private key', async () => {
+    const state = await generateEncryptedFormLocalState(TEST_KEY_PAIR.publicKey)
+    const submission = encryptFormData(TEST_DATA, state)
+    const decrypted = decryptFormData(state.sodium, submission, {
+      algorithm: 'sealedBox',
+      ...TEST_KEY_PAIR,
+    })
+    expect(decrypted).toEqual(TEST_DATA)
+  })
+
+  test('encrypting data, decryption via local state (edition)', async () => {
+    const state = await generateEncryptedFormLocalState(TEST_KEY_PAIR.publicKey)
+    const submission = encryptFormData(TEST_DATA, state)
+    const decrypted = decryptFormForEdition(submission, state)
+    expect(decrypted).toEqual(TEST_DATA)
+  })
+})
